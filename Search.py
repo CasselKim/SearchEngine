064c3654311f1b7ec1ffecfaaa68873941fd5be1
CasselKim <seongr39@gmail.com>
2022-05-20 01:12:14 +0900

    Update Search.py : make term dictionary

--- Search.py
+++ Search.py
@@ -1,18 +1,33 @@
 from glob import glob
 import xml.etree.ElementTree as ET
-from kiwipiepy import Kiwi
-kiwi = Kiwi()
-
+from konlpy.tag import Mecab
+from collections import defaultdict, Counter
 
 # get corpus data
-files = glob('corpus\*.xml')
+collection = sorted(glob('corpus/*.xml'))
+print(collection)
+
+# parsing xml files
 docs = []
-for f in files : 
-    tree = ET.parse(f)
+for doc in collection : 
+    tree = ET.parse(doc)
     content = tree.find("title").text + '\n' + tree.find("text").find("p").text
     docs.append(content)
-    
 print(docs)
+
+# change into morphs, make dictionary using Mecab
+D = defaultdict(list)
+mecab = Mecab()
+for i,x in enumerate(docs) : 
+    terms = mecab.morphs(x)
+    counter = Counter(terms)
+    for term in counter : 
+        D[term].append([i,counter[term]])
+print(D)   
+
+
+
+
 
 
     
